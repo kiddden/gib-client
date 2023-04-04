5ef38e67e524fb5c9da593fbd1b60f7d35c1369d
kiddden <kazzer@knu.ua>
2023-04-04 17:08:53 +0300

    Initial Commit

---
+++ gib-client/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  gib-client
+//
+//  Created by Eugene Ned on 04.04.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gib-client/gib_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  gib_clientApp.swift
+//  gib-client
+//
+//  Created by Eugene Ned on 04.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct gib_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
